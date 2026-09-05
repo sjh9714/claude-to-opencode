e528e59a27802372694cc49fca67be64303db5ee
sjh9714 <163989462+sjh9714@users.noreply.github.com>
2026-09-05 12:27:42 +0900

    patch

--- test/test.mjs
+++ test/test.mjs
@@ -18,6 +18,7 @@ assert.deepStrictEqual(packageManifest.dsh.compatibility.dshReleases, {
   '0.1.2-alpha.3': 'compatible',
   '0.1.2-alpha.4': 'compatible',
   '0.1.2-alpha.5': 'compatible',
+  '0.1.2-rc.1': 'compatible',
 });
 assert.deepStrictEqual(packageManifest.dsh.compatibility.profiles, ['web']);
 assert.deepStrictEqual(packageManifest.os, ['darwin', 'linux', 'win32']);
